0ba62e31a4c431ec2f4c5f9412259c51c1153335
CorentinDelclaud <corentin.delclaud@gmail.com>
2026-01-06 10:16:46 +0100

    test : creation d'un test pour la creation des displines

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Projet_PFE
 Projet fin d'étude
+
+Convention pour les messages de commit : 
+build
+chore
+ci
+docs
+feat
+fix
+perf
+refactor
+revert
+style
+test
--- classes/stage.py
+++ classes/stage.py
@@ -1,6 +1,7 @@
 class stage:
-    debut_stage : int
-    fin_stage : int
+    debut_stage : int #semaine de debut du stage
+    fin_stage : int #semaine de fin du stage
+    
     def __init__(self, debut_stage: int, fin_stage: int):
         self.debut_stage = debut_stage
         self.fin_stage = fin_stage
