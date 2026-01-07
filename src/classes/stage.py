75d18d1dbf3749f4c296a1334b47e10e1cd38b64
CorentinDelclaud <corentin.delclaud@gmail.com>
2026-01-07 14:31:01 +0100

    feat : liste d'appel et stage

--- src/classes/stage.py
+++ src/classes/stage.py
@@ -1,7 +1,12 @@
 class stage:
+    id_stage : int = -1
+    nom_stage : str
     debut_stage : int #semaine de debut du stage
     fin_stage : int #semaine de fin du stage
     
-    def __init__(self, debut_stage: int, fin_stage: int):
+    def __init__(self, nom_stage: str, debut_stage: int, fin_stage: int):
+        self.id_stage = stage.id_stage + 1
+        stage.id_stage += 1
+        self.nom_stage = nom_stage
         self.debut_stage = debut_stage
         self.fin_stage = fin_stage
