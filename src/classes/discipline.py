7102c824b6f68376b862946eed7388221d8c8761
CorentinDelclaud <corentin.delclaud@gmail.com>
2026-01-21 09:19:37 +0100

    feature : deuxième version du modele

--- src/classes/discipline.py
+++ src/classes/discipline.py
@@ -6,16 +6,16 @@ class discipline:
     quota : list[int] = [0] * 3  # Default quota list for each year
     presence : list[bool] = [False] * 10  # Default presence list with 10 False values
     annee : list[int] = [4,5,6]  # Default to all years
-    frequence_vacations : int = 0  # Fréquence des vacations à effectuer pour l’élève Toutes les X semaines : number input (1, 2, 3...) 1 = chaque semaine 2 = une semaine sur deux 3 = une semaine sur trois
-    nb_vacations_par_semaine : int = 0  # Nombre de vacations par semaine à effectuer pour l’élève
+    frequence_vacations : int = 0  # Fréquence des vacations à effectuer pour l’élève Toutes les X semaines : number input (1, 2, 3...), 0 = pas de contrainte, 1 = chaque semaine 2 = une semaine sur deux 3 = une semaine sur trois
+    nb_vacations_par_semaine : int = 0  # Nombre de vacations par semaine à effectuer pour l’élève, 0 = pas de contrainte
     repartition_semestrielle : list[int] | None = None  # Répartition semestrielle du total des quotas [semestre1, semestre2] semestre1 + semestre2 = total des quotas
-    paire_jours : list[(int, int )] | None = None  # indiquer que les vacations doivent être planifiées par paire sur les jours (Lundi, Mercredi) et (Mardi, Jeudi) -> [(0,2),(1,3)]
+    paire_jours : list[(int, int )] | None = None  # indiquer que les vacations doivent être planifiées par paire sur les jours (Lundi, Mercredi) ou (Mardi, Jeudi) -> [(0,2),(1,3)]
     mixite_groupes : int = 0  # Composition des groupes Mixité des niveaux  0 = Pas de contrainte, 1 = Exactement 1 élève de chaque niveau, 2 = Au moins 2 niveaux différents par vacation, 3 = Tous du même niveau
     repetition_continuite : int = 0  # Contrainte de continuité Éviter répétitions : 0 = Pas de contrainte 1 = Pas 2 fois de suite 2 = Pas 3 fois de suite 3 = Jamais plus de X fois (préciser X)
     priorite_niveau : list[int] | None = None # index 0 1st prio , index 1 2nd prio, index 2 3eme prio ( value 4,5,6 for year)
     remplacement_niveau : list[(int,int,int)]   # [X,Y,Z] with X année absente, Y année remplacement et Z quota  X,Y -> (4A, 5A, 6A)  quota -> number of students to replace at the vacation
     take_jour_pref : bool = False  # Prendre en compte les jours préférentiels de l'élève
-    
+    be_filled : bool = False  # Discipline impérativement remplie sur ces vacations
     def __init__(self, id_discipline: int, nom_discipline: str, nb_eleve: list[int], en_binome: bool, quota: list[int], presence: list[bool] = None, annee: list[int] = None, **kwargs):
         self.id_discipline = id_discipline
         self.nom_discipline = nom_discipline
@@ -134,3 +134,5 @@ class discipline:
         self.remplacement_niveau = remplacements
     def modif_take_jour_pref(self, take: bool):
         self.take_jour_pref = take
+    def modif_fill_requirement(self, be_filled: bool):
+        self.be_filled = be_filled
